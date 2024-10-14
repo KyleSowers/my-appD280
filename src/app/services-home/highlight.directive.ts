e9b9602b19a2e23aee0c3f6aa13395862bf8d8b4
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-14 18:43:14 -0400

    Creates testing of debugger used in the highlight.directive.ts file and run to see the Developer Tools result/action.

--- src/app/services-home/highlight.directive.ts
+++ src/app/services-home/highlight.directive.ts
@@ -6,12 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightDirective {
   constructor(el: ElementRef) {
-    try {
-      console.log(el);
-      el.nativeElement.style.backgroundColor = 'purple';
-      throw new Error('An error has occurred');
-    } catch (e) {
-      console.error(e);
-    }
+    debugger;
+    el.nativeElement.style.backgroundColor = 'purple';
   }
 }
