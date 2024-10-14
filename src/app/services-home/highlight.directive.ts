6ecb632bfcb4c4521076e74759059bc5435c7705
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-14 16:18:03 -0400

    Creates an error through the addition of a try-catch in the constructor for demonstration on debugging in angular.

--- src/app/services-home/highlight.directive.ts
+++ src/app/services-home/highlight.directive.ts
@@ -6,6 +6,12 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightDirective {
   constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'purple';
+    try {
+      console.log(el);
+      el.nativeElement.style.backgroundColor = 'purple';
+      throw new Error('An error has occurred');
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
