ebc441b3d0b60cbba19f73a6752ce5929668d266
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-13 21:03:46 -0400

    New ProjectsHomeComponent created.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeHomeComponent } from './home-home/home-home.component';
 import {ContactHomeComponent} from './contact-home/contact-home.component';
+import {ProjectsHomeComponent} from './projects-home/projects-home.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeHomeComponent },
   { path: 'contact', component: ContactHomeComponent },
+  { path: 'projects', component: ProjectsHomeComponent},
 ];
