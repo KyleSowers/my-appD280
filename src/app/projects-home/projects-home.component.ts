739b557fe87c77cebc49a833409d899735bb2d4c
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-17 11:15:50 -0400

    Demonstrates/Adds two-way binding to the projects-home.component.html file card to add user information to card.

--- src/app/projects-home/projects-home.component.ts
+++ src/app/projects-home/projects-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-projects-home',
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './projects-home.component.css'
 })
 export class ProjectsHomeComponent {
+  firstName: string = "William";
+  lastName: string = "Wallace";
+  phoneNumber: string = "(201) 748-6000";
 
+  setProperty(event: Event) {
+    let target = event.target as HTMLInputElement
+    let property: string = target.id
+    switch(property) {
+      case "firstNameInput":
+        this.firstName = target.value;
+        break;
+      case "lastNameInput":
+        this.lastName = target.value;
+        break;
+      case "phoneNumberInput":
+        this.phoneNumber = target.value;
+        break;
+      default:
+        console.warn("No matching ID found!")
+        break;
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {  }
 }
