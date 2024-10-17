d509e24c0dab667f11fb61067e015a80c3bd1788
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-16 21:24:15 -0400

    Demonstrates event binding through contact-home.component.html file cancel button and export method in contact-home.component.ts file.

--- src/app/contact-home/contact-home.component.ts
+++ src/app/contact-home/contact-home.component.ts
@@ -10,8 +10,15 @@ import {Component, OnInit} from '@angular/core';
 export class ContactHomeComponent implements OnInit{
   isUnchanged = true;
 
+  onCancel(event?: KeyboardEvent) {
+    const evtMsg = event ? ' Event target is ' + (event.target as HTMLElement).textContent : '';
+    alert('Canccelled.' + evtMsg);
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  protected readonly oncancel = oncancel;
 }
